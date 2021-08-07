9a092a06e7091ea31e286662bd60b369de6f51bc
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2021-08-07 20:27:47 +0100

    Start of analyse code and tests.

--- src/jerzywie/analyse.clj
+++ src/jerzywie/analyse.clj
@@ -3,7 +3,9 @@
             [java-time :as j]
             [java-time.interval :as ji]))
 
-(def name-cache (atom []))
+(def name-cache (atom {}))
+
+(def empty-name {:names #{} :group nil :filterby nil})
 
 (def bank-credit "Bank credit")
 (def transfer-from "Transfer from")
@@ -19,6 +21,17 @@
         strip-desc (strip-prefix desc)
         group (if (not= name strip-desc) strip-desc nil)]
     {:name name :group group}))
+
+(defn make-key [{:keys [name group]}]
+  (hash (if (nil? group) name group)))
+
+(defn cache-name [{:keys [name group] :as m}]
+  (let [key (make-key m)
+        value (get @name-cache key empty-name)
+        new-value (assoc value :names (conj (:names value) name)
+                         :group group
+                         :filterby (if (nil? group) :names :group))]
+    (swap! name-cache assoc key new-value)))
 
 (defn process-income [transactions]
   (let [in-txns (filter #(nil? (:out %)) transactions)]
--- test/jerzywie/analyse_test.clj
+++ test/jerzywie/analyse_test.clj
@@ -24,3 +24,51 @@
       {:type "Transfer from FRED BLOGGS" :desc "112233 78903456"}
       {:name "FRED BLOGGS" :group "112233 78903456"})))
 
+(defn cache-two-unrelated [do-reset?]
+  (if do-reset? (reset! sut/name-cache {}))
+  (sut/cache-name {:name "A" :group nil})
+  (sut/cache-name {:name "B" :group nil}))
+
+(defn cache-two-related [do-reset?]
+  (if do-reset? (reset! sut/name-cache {}))
+  (sut/cache-name {:name "C" :group "c-group"})
+  (sut/cache-name {:name "D" :group "c-group"}))
+
+(deftest cache-name-tests
+  (testing "cache-name caches correctly.1"
+    (are [map func result] (= (func (sut/cache-name map)) result)
+      {:name "UNA" :group nil}
+      (fn [r] (-> (vals r) first :names (contains? "UNA")))
+      true
+
+      {:name "DUO" :group nil}
+      (fn [r] (-> (vals r) count))
+      2
+
+      {:name "TRIO" :group "AGROUP"}
+      (fn [r] (-> (vals r) count))
+      3))
+
+  (testing "cache-name caches unrelated names correctly"
+    (let [cache (cache-two-unrelated true)]
+      (is (= (-> cache vals count) 2))
+      (is (nil? (->> cache vals second :group)))
+      (is (= (-> cache vals second :filterby) :names))))
+
+  (testing "cache-name caches related names correctly"
+    (let [cache (cache-two-related true)]
+      (is (= (-> cache vals count) 1))
+      (is (= (->> cache vals first :group) "c-group"))
+      (is (= (-> cache vals first :filterby))) :group))
+
+  (testing "more cache-name tests"
+    (let [_ (cache-two-related true)
+          cache (cache-two-unrelated false)
+          cache2 (cache-two-unrelated false)]
+      (is (= (-> cache vals count) 3))
+      (is (= (-> cache vals first :group) "c-group"))
+      (is (= (-> cache vals first :filterby) :group))
+      (is (= (-> cache vals first :names count) 2))
+      (is (= (-> cache vals second :names count) 1))
+      (is (nil? (-> cache vals second :group)))
+      (is (= cache cache2)))))
