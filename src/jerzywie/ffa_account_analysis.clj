d20ba3c4aa95ce047a66695fca4e220f55ac8234
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2021-09-14 12:15:04 +0100

    Improve command-line processing; add help.

--- src/jerzywie/ffa_account_analysis.clj
+++ src/jerzywie/ffa_account_analysis.clj
@@ -6,8 +6,12 @@
              [report :as rep]
              [util :as u]]
             [java-time :as j]
-            [clojure.pprint :as pp])
+            [clojure.pprint :as pp]
+            [clojure.string :as string])
   (:gen-class))
+
+(def local-file "resources/test-transactions.csv")
+(def help-arg "-h")
 
 (defn process-transactions [date transactions]
   (->> transactions
@@ -15,24 +19,46 @@
        alloc/process-income
        (anal/analyse-donations date)))
 
+(defn print-help []
+  (->> ["FFA Bank account transaction analysis."
+        "Usage: transactions-csv yyyy mm dd"
+        ""
+        "Options:"
+        " -h prints this message."
+        " transactions-csv = '_' defaults to test transactions file."
+        ""
+        "If no date is supplied the default is today."]
+       (string/join \newline)
+       (println)))
+
+(defn process-args [args]
+  (let [nargs (count args)
+        first-arg (if (> nargs 0) (first args) help-arg)]
+    (if (= first-arg help-arg)
+      (do
+        (print-help)
+        {:help-given true})
+      (let [file-name (if (= first-arg "_") local-file first-arg)
+            y (if (> nargs 1) (Integer/parseInt (nth args 1)) 0)
+            m (if (> nargs 2) (Integer/parseInt (nth args 2)) 0)
+            d (if (> nargs 3) (Integer/parseInt (nth args 3)) 0)
+            date (if (> (+ y m d) 3) (j/local-date y m d) (j/local-date))]
+        {:file-name file-name :date date}))))
+
 (defn -main
   "Analyse transactions in downloaded transactions csv-file."
   [& args]
-  (let [nargs (count args)
-        local-file "resources/test-transactions.csv"
-        file-arg (if (> nargs 0) (first args) local-file)
-        file-name (if (= file-arg "_") local-file file-arg)
-        y (if (> nargs 1) (Integer/parseInt (nth args 1)) 0)
-        m (if (> nargs 2) (Integer/parseInt (nth args 2)) 0)
-        d (if (> nargs 3) (Integer/parseInt (nth args 3)) 0)
-        date (if (> (+ y m d) 3) (j/local-date y m d) (j/local-date))
-        processed-txns (->> {:filename file-name}
-                            (csv/get-statement-data)
-                            (process-transactions date))]
-    (println "\nDonations at" date)
-    (println "\nCurrent donations")
-    (pp/pprint (filter #(contains? % :current) processed-txns))
-    (println "\n One-offs")
-    (pp/pprint (filter #(contains? (:freq %) :one-off) processed-txns))
-    (println "\n All donors")
-    (pp/pprint (rep/donor-report))))
+  (let [{:keys [file-name date help-given]} (process-args args)]
+    (if (not help-given)
+      (let [processed-txns (->> {:filename file-name}
+                                (csv/get-statement-data)
+                                (process-transactions date))]
+
+        (println "\nDonations at" date)
+        (println (format "Transactions file '%s'" file-name))
+        (println "\nCurrent donations")
+        (pp/pprint (filter #(contains? % :current) processed-txns))
+        (println "\nOne-offs")
+        (pp/pprint (filter #(contains? (:freq %) :one-off) processed-txns))
+        (println "\nAll donors")
+        (pp/pprint (rep/donor-report))))))
